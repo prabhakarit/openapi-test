c909a0ec633f7a71994b24149b359e66c176c135
prabhakarit <prabhakar.borah@gmail.com>
2023-03-13 07:07:23 +0000

    Auto-commit: terraform main script to create AWS s3 website bucket

--- s3-website-infra/main.tf
+++ s3-website-infra/main.tf
@@ -24,15 +24,12 @@ resource "aws_s3_bucket_policy" "website" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid = "PublicReadGetObject"
         Effect = "Allow"
         Principal = "*"
         Action = [
           "s3:GetObject"
         ]
-        Resource = [
-          "${aws_s3_bucket.website.arn}/*"
-        ]
+        Resource = "${aws_s3_bucket.website.arn}/*"
       }
     ]
   })
