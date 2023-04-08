5eb7835e2b655cd8dd75f1ac537ece7dbb5f305d
prabhakarit <prabhakar.borah@gmail.com>
2023-04-09 00:10:30 +0530

    added for cli based python run

---
+++ openai-cli/main.py
@@ -0,0 +1,27 @@
+import argparse
+import csv
+import openai
+import os
+import sys
+from pathlib import Path
+import shutil
+import os
+import uuid
+
+parser=argparse.ArgumentParser()
+#parser.add_argument('multi_word')
+parser.add_argument('-q', '--query',nargs='+', required=False, action='store', dest='query', default=False, help="provide free text query")
+parser.add_argument('-p', '--planfile',nargs='+', required=False, action='store', dest='planfile', default=False, help="provide planfile to generate terraform script")
+# parser.add_argument('-o', '--outputdir', nargs='+', required=False, action='store', dest='outputdir', default=False, help="provide output directory name")
+# parser.add_argument('-p', '--preference', nargs='+', required=True, action='store', dest='preference', default=False, help="provide devops preference")
+
+args=parser.parse_args()
+queryText = args.query
+freeTextQuery=queryText[0]
+
+randomised_op_folder_name = './'+str(uuid.uuid4().hex)
+isExist = os.path.exists(randomised_op_folder_name)
+if not isExist:
+    os.mkdir(randomised_op_folder_name)
+# print(freeTextQuery)
+os.system("aiac " + freeTextQuery + ' --output-file='+randomised_op_folder_name+'/main.tf ' + ' --readme-file='+randomised_op_folder_name+'/readme.md ' + ' --model="gpt-3.5-turbo-0301"')
